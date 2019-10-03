edcd553857ad519e1dc0a00272a81337bb06b64d
Kirill V <kirilliuswest@gmail.com>
2019-10-03 18:24:01 -0400

    modified main.js so that the script validates the result and verifies that at least one character of each valid type is there

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,13 @@ having received the answers, generate the password corresponding to the criteria
 Button "Copy to clipboard" will copy the password from text area into clipboard
 */
 
+// variables to store types of values
+
+allLower = 'abcdefghijklmnopqrstuvwxyz';
+allUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+allSpecial = '!@#$^&%*()+=-[]\/{}|:<>?,.';
+allNumbers = '1234567890';
+
 // functions to get and validate user input. If user input does not pass validation, notify user and try again
 
 var inputLength = function () {
@@ -37,10 +44,6 @@ var confirmTypes = function () {
 // functions to generate character set and password
 
 var generateCharSet = function (types) {
-    allLower = 'abcdefghijklmnopqrstuvwxyz';
-    allUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    allSpecial = '!@#$^&%*()+=-[]\/{}|:<>?,.';
-    allNumbers = '1234567890';
     charSet = '';
     types[0] ? charSet = charSet + allSpecial : charSet = charSet;
     types[1] ? charSet = charSet + allNumbers : charSet = charSet;
@@ -49,13 +52,49 @@ var generateCharSet = function (types) {
     return charSet;
 }
 
-var generatePwd = function (pwdLength, charSet) {
+var generatePwd = function (pwdLength, charSet, types) {
     pwd = '';
     for (i = 0; i <= pwdLength - 1; i++) {
         pwd = pwd + charSet[Math.floor(Math.random() * charSet.length)]; //getting a random element of a character set
     }
-    return pwd;
+    //validate that the password has all necessary types of values. If doesn't, rerun
+    if (validate(pwd, types)) {
+        return pwd;
+    }
+    else {
+        return generatePwd();
+    }
 }
+// validation function for a password
+
+var validate = function (pwd, valTypes) {
+    var check0 = false;
+    var check1 = false;
+    var check2 = false;
+    var check3 = false;
+    valTypes[0] ? check0 = isPartOf(pwd, allSpecial) : check0 = true;
+    valTypes[1] ? check1 = isPartOf(pwd, allNumbers) : check1 = true;
+    valTypes[2] ? check2 = isPartOf(pwd, allLower) : check2 = true;
+    valTypes[3] ? check3 = isPartOf(pwd, allUpper) : check3 = true;
+    if (check0 && check1 && check2 && check3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+var isPartOf = function (pwd, charSet) {
+    for (char of pwd) {
+        if (charSet.includes(char) ) {
+            return true; 
+        } 
+    } 
+    return false;
+} 
+
+
 
 // functions for buttons
 
@@ -63,7 +102,7 @@ var buttonGeneratePwd = function () {
     pwdLength = inputLength();
     types = confirmTypes();
     charSet = generateCharSet(types);
-    pwd = generatePwd (pwdLength, charSet);
+    pwd = generatePwd (pwdLength, charSet, types);
     document.getElementById("output").innerHTML = pwd;
 }
 
